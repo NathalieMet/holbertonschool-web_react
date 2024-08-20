7003ed7053abab6a9d417509487bd116196d04a3
nathalie <nathalie.metreau@hotmail.fr>
2024-08-20 13:01:49 +0200

    add task 1

---
+++ react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,11 @@
+import './Notifications.css';
+import React from 'react';
+
+export function Notifications() {
+	return(
+		<div className='Notifications'>
+		<p>Here is the list of notifications</p>
+		</div>
+		);
+}
+
