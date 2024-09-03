0a8934db3b89bb9001ceea7b0d244650a15194c6
nathalie <nathalie.metreau@hotmail.fr>
2024-09-03 21:43:54 +0200

    add task 8

--- React_component/task_3/dashboard/src/HOC/WithLogging.js
+++ React_component/task_3/dashboard/src/HOC/WithLogging.js
@@ -3,12 +3,20 @@ import React, { Component } from 'react';
 const WithLogging = WrappedComponent => {
 	class WithLoggingHOC extends Component {
 		componentDidMount() {
-			console.log(`Component ${WrappedComponent.displayName || WrappedComponent.name || 'Component'} is mounted`);
+			const name = WrappedComponent.displayName || WrappedComponent.name;
+			if (name) {
+			console.log(`Component ${name} is mounted`);
+			}
+			console.log(`Component is mounted`);
 		}
 
 		componentWillUnmount() {
-			console.log(`Component ${WrappedComponent.displayName || WrappedComponent.name || 'Component'} is going to unmount`);
+			const name = WrappedComponent.displayName || WrappedComponent.name;
+			if (name) {
+			console.log(`Component ${name} is going to unmount`);
 		}
+		console.log(`Component is going to unmount`);
+	}
 
 		render() {
 			return <WrappedComponent {...this.props} />;
