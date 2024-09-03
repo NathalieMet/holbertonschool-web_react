76a56927ee3359df06e26da955289f6f20e02485
nathalie <nathalie.metreau@hotmail.fr>
2024-09-03 21:50:20 +0200

    add tasks in task_4

---
+++ React_component/task_4/dashboard/src/App/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './App.css'
+import Notifications from '../Notifications/Notifications.js'
+import Body from '../Login/Login.js'
+import Footer from '../Footer/Footer.js'
+import Header from '../Header/Header.js'
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList.js'
+import { getLatestNotification } from '../utils/utils.js';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom.js';
+import BodySection from '../BodySection/BodySection.js';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.handleKeyDown = this.handleKeyDown.bind(this); // Bind the event handler
+	  }
+
+	  componentDidMount() {
+		// Add the event listener when the component is mounted
+		window.addEventListener('keydown', this.handleKeyDown);
+	  }
+
+	  componentWillUnmount() {
+		// Remove the event listener when the component is about to unmount
+		window.removeEventListener('keydown', this.handleKeyDown);
+	  }
+
+	  handleKeyDown(event) {
+		if (event.ctrlKey && event.key === 'h') {
+		  alert('Logging you out');
+		  this.props.logOut();
+		}
+	  }
+
+	render() {
+		const { isLoggedIn } = this.props;
+
+		const listCourses = [
+			{ id: 1, name: 'ES6', credit: 60 },
+			{ id: 2, name: 'Webpack', credit: 20 },
+			{ id: 3, name: 'React', credit: 40 },
+		];
+
+		const listNotifications = [
+			{ id: 1, type: "default", value: "New course available"},
+				{ id: 2, type: "urgent", value: "New resume available"},
+				{ id: 3, html: { __html: getLatestNotification() }, type: "urgent"}
+		];
+
+		return (
+			<React.Fragment>
+				<Notifications listNotifications={listNotifications}/>
+				<div className="App">
+					<Header />
+					{isLoggedIn ?
+					<BodySectionWithMarginBottom title={"Course list"}><CourseList listCourses={listCourses} /> </BodySectionWithMarginBottom>
+					: <BodySectionWithMarginBottom title={"Log in to continue"}><Body /></BodySectionWithMarginBottom>}
+					<Footer />
+				</div>
+				<BodySection title={"News from the School"} children={"some text"}/>
+			</React.Fragment>
+		);
+	}
+}
+
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+	logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+	isLoggedIn: false,
+	logOut: () => {},
+};
+
+export default App;
---
+++ React_component/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Notifications from './Notifications';
+
+class NotificationItem extends Component {
+  render() {
+		const { id, type, html, value, markAsRead } = this.props;
+  if (html) {
+    return <li className={type} data-notification-type={type} dangerouslySetInnerHTML={html} onClick={() => markAsRead(id)}></li>;
+  }
+
+  return <li className={type} data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li>;
+}
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+};
+
+export default NotificationItem
